cde292e42b204aaaa07cb4da12de7deddb4d94cb
Hao Hoang <haohoangofficial@gmail.com>
2022-06-18 08:00:30 +0700

    version file

---
+++ super_request/__version__.py
@@ -0,0 +1,10 @@
+__title__ = "super request"
+__description__ = "Make a request very simple."
+__url__ = ""
+__version__ = "1.0.0"
+# __build__ = 0x022800
+__author__ = "Hoang Hao"
+__author_email__ = "haohoangofficial@gmail.com"
+__license__ = "Apache 2.0"
+__copyright__ = "Copyright 2022 Hoang Hao"
+__cake__ = ""
